178c5cc3b2ecf363251c05f2090b30f8e6f8d3c2
Eric McKinney <mckinney99@gmail.com>
2018-01-04 22:00:58 +0000

    automatically backed up by learn

--- db/migrate/20180104215839_change_string_to_integer_year.rb
+++ db/migrate/20180104215839_change_string_to_integer_year.rb
@@ -1,6 +1,6 @@
 class ChangeStringToIntegerYear < ActiveRecord::Migration
   def change
-    remove_column :year_completed, :landmarks
+    remove_column :landmarks, :year_completed
     add_column :year_completed, :landmarks, :integer
   end
 end
